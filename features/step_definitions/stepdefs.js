139ef365e32b8e06a25be89ba2bfc84f6ec996b4
Frederico Chaves <frederico.chaves@gmail.com>
2020-09-04 22:02:21 -0300

    Primeiros cenarios ok, tutorial simples

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,18 @@
-import assert from 'assert';
-import { Given, When, Then } from 'cucumber';
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+const isItFriday = today => {
+    return '' + this.today == 'Friday';
+};
+
+Given('today is {string}', (expectedAnswer) => {
+    this.today = expectedAnswer;
+});
+
+When('I ask whether it\'s Friday yet', () => {
+    this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', expectedAnswer => {
+    assert.equal(this.actualAnswer, JSON.parse(expectedAnswer));
+});
